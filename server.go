49b2bc38de8b26f0233b33c229eccef85f6df3b7
Bryan Davidson <bryan.davidson@rackspace.com>
2015-08-06 11:48:08 -0400

    Preliminary GET endpoint

--- server.go
+++ server.go
@@ -1,13 +1,37 @@
 package main
 
 import (
-  "github.com/go-martini/martini"
+    "net/http"
+
+    "github.com/go-martini/martini"
+    // "github.com/user/gorse-cache-redis"
+    // "github.com/user/gorse-handler-auth"
+    // "github.com/user/gorse-storage-kafka"
+    // "github.com/user/gorse-storage-redis"
 )
 
 func main() {
-  m := martini.Classic()
-  m.Get("/", func() string {
-    return "Hello world!"
-  })
-  m.Run()
+    m := martini.Classic()
+
+    m.Handlers()
+
+    m.Get("/**", func(r *http.Request, params martini.Params) string {
+        topic := params["_1"]
+        marker, marker_present := r.URL.Query()["marker"]
+
+        // Get message from storage
+        // if marker_present {
+        //     storage.Get(topic, marker)
+        // } else {
+        //     storage.Get(topic)
+        // }
+
+        tempReturn := "Getting messages on " + topic
+        if marker_present {
+            tempReturn += " with marker of " + marker[0]
+        }
+        return tempReturn + "\r\n"
+    })
+
+    m.Run()
 }
